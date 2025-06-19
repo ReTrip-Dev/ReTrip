95f8e8675fcaf47333243139be813ac65e7b272c
Yongbum Kim <dt10002@naver.com>
2025-06-20 02:10:54 +0900

    Refactor: 회원 기능 비활성화

--- src/components/RetripReportGenerator.vue
+++ src/components/RetripReportGenerator.vue
@@ -2,11 +2,11 @@
     <div class="photo-upload-page">
         <header class="header">
             <div class="logo-area">
-                <a href="/photo">
+                <a href="/">
                     <img src="/src/assets/logo(1).png" alt="ReTrip Logo" class="retrip-logo-icon">
                 </a>
             </div>
-            <a href="/history" class="nav-link">히스토리</a>
+            <!-- 히스토리 기능 연결 <a href="/history" class="nav-link">히스토리</a> -->
         </header>
 
         <div class="container">
